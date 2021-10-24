b3316eb46d16ebbec23851e2295ab3bd6c9c3bf3
ZbxrB <92222264+ZbxrB@users.noreply.github.com>
2021-10-24 21:00:40 +0300

    Delete homework_4_2.py

--- homework_4/homework_4_2.py
+++
@@ -1,5 +0,0 @@
-basic_list = [12, 46, 13, 1, 11, 44, 45, 12, 10, 56, 3, 2, 4, 5]
-
-parsed_list = [basic_list[i] for i in range(1, len(basic_list)) if basic_list[i] > basic_list[i-1]]
-
-print(parsed_list)
